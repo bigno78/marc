9f07dfe39107a3a0c4ea232fdb1a51b7260d0627
Miroslav Demek <xdemek@fi.muni.cz>
2022-05-24 13:30:15 +0200

    Add custom implementation of integer parsing

--- src/parsing/reader.hpp
+++ src/parsing/reader.hpp
@@ -3,6 +3,28 @@
 #include <string>
 #include <istream>
 
+/**
+ * Extracts and integers from `str` starting from index `i`.
+ * If the integer doesn't fit into size_t return false,
+ * othervise returns true.
+ */
+bool read_integer(const std::string& str, size_t i, size_t& val, size_t& end) {
+    size_t res = 0;
+    
+    while(i < str.size() && isdigit(str[i])) {
+        size_t new_res = val*10 + (str[i] - '0');
+        if (new_res < res) {
+            return false;
+        }
+        ++i;
+    }
+
+    end = i;
+    val = res;
+
+    return true;
+}
+
 template<typename DataCollector>
 bool read_entry(const std::string& str, DataCollector& collector) {
     size_t i = 0;
