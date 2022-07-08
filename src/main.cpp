b2cb3a5238e39e1e1dc1636656208bc72718dd70
Miroslav Demek <xdemek@fi.muni.cz>
2022-07-08 14:47:36 +0200

    Make verbose output a bit more readable

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,29 @@ void print_parsing_error(const Status& status) {
     std::cerr << "Error:" << status.line << ":" << status.col << ": " << status.error_message << "\n";
 }
 
+void print_matrix_info(const Header& header) {
+    std::cout << "Matrix parameters:\n";
+    std::cout << "    rows:     " << header.rows << "\n";
+    std::cout << "    cols:     " << header.cols << "\n";
+    std::cout << "    entries:  " << header.entries << " (these are only entries in the file and don't account for symmetry)\n";
+    std::cout << "    symmetry: ";
+    switch (header.symmetry) {
+        case Symmetry::general:
+            std::cout << "general (no symmetry)";
+            break;
+        case Symmetry::symmetric:
+            std::cout << "symmetric";
+            break;
+        case Symmetry::skew_symmetric:
+            std::cout << "skew-symmetric";
+            break;
+        case Symmetry::hermitian:
+            std::cout << "hermitian";
+            break;
+    }
+    std::cout << "\n\n";
+}
+
 ImageConfig init_image_config(const Header& header, const CmdOptions& opts) {
     ImageConfig config;
 
@@ -47,9 +70,12 @@ Grid make_grid(const Header& header, const ImageConfig& config, const CmdOptions
     Grid grid(header, max_blocks_y, max_blocks_x);
 
     if (opts.verbose) {
-        std::cout << "Grid: size " << grid.rows() << " x " << grid.cols();
-        std::cout << " with " << grid.block_size() << " x " << grid.block_size() << " blocks";
-        std::cout << " of capacity " << grid.block_capacity() << "\n";
+        std::cout << "Grid parameters:\n";
+        std::cout << "    rows:           " << grid.rows() << "\n";
+        std::cout << "    cols:           " << grid.cols() << "\n";
+        std::cout << "    block size:     " << grid.block_size() << "\n";
+        std::cout << "    block capacity: " << grid.block_capacity() << "\n";
+        std::cout << "\n";
     }
 
     return grid;
@@ -61,8 +87,13 @@ void draw_grid(const Grid& grid, ImageConfig& image_config, const CmdOptions& op
     image_config.height = grid.rows() * image_config.block_size + 2*image_config.border_size;
 
     if (opts.verbose) {
-        std::cout << "Image: size " << image_config.width << " x " << image_config.height;
-        std::cout << " with " << image_config.block_size << " x " << image_config.block_size << " blocks\n";
+        std::cout << "Image parameters:\n";
+        std::cout << "   max_width:     " << image_config.viewport_width << "\n";
+        std::cout << "   max_height:    " << image_config.viewport_height << "\n";
+        std::cout << "   width:         " << image_config.width << "\n";
+        std::cout << "   height:        " << image_config.height << "\n";
+        std::cout << "   adjust colors: " << (image_config.adjust_colors ? "on" : "off") << "\n";
+        std::cout << "\n";
     }
 
     SvgDrawer svg;
@@ -93,24 +124,7 @@ int main(int argc, char** argv) {
     }
 
     if (opts->verbose) {
-        std::cout << "Matrix of size " << header.rows << " x " << header.cols << " with " << header.entries << " entries\n";
-        std::cout << "Symmetry: ";
-        switch (header.symmetry) {
-            case Symmetry::general:
-                std::cout << "general\n";
-                break;
-            case Symmetry::symmetric:
-                std::cout << "symmetric\n";
-                break;
-            case Symmetry::skew_symmetric:
-                std::cout << "skew-symmetric\n";
-                break;
-            case Symmetry::hermitian:
-                std::cout << "hermitian\n";
-                break;
-            default:
-                std::cout << "Unknown symmetry\n";
-        }
+        print_matrix_info(header);
     }
 
     ImageConfig image_config = init_image_config(header, *opts);
@@ -120,6 +134,10 @@ int main(int argc, char** argv) {
     if (!status) {
         print_parsing_error(status);
         return EXIT_FAILURE;
+    }
+
+    if (opts->verbose) {
+        std::cout << "Entries processed: " << grid.entries() << "\n\n";
     }
 
     draw_grid(grid, image_config, *opts);
