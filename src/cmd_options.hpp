d45dbaa5aa2fa3c74ce4eabb2fbafe6788dbb4ce
Miroslav Demek <xdemek@fi.muni.cz>
2022-07-08 15:03:46 +0200

    Add usage

--- src/cmd_options.hpp
+++ src/cmd_options.hpp
@@ -17,6 +17,18 @@ struct CmdOptions {
     bool adjust_colors = false;
 };
 
+void print_usage(const std::string& executable_name) {
+    std::cout << "Usage: " << executable_name << " <input_file.mtx> -o <output_file.svg>\n";
+    std::cout << "Options:\n";
+    std::cout << "    -o <file>                       The filename of the output image. Default is 'out.svg'.\n";
+    std::cout << "    -v                              Enables verbose output.\n";
+    std::cout << "    -h, --help                      Print usage and exit.\n";
+    std::cout << "    -w <width>, --width <width>     The maximum width of the image. The actual width can be smaller.\n";
+    std::cout << "    -h <height>, --height <height>  The maximum height of the image. The actual height can be smaller.\n";
+    std::cout << "    -a, --adjust-colors             Compute colors based on the maximum occupancy of blocks "
+                                                     "instead of based on block capacity.\n";
+}
+
 std::optional<size_t> parse_integer_argument(std::string_view arg_name, std::string_view arg_val) {
     std::string arg_string(arg_val);
     
@@ -55,6 +67,9 @@ std::optional<CmdOptions> parse_args(int argc, char** argv) {
                 return std::nullopt;
             }
             opts.output_filename = argv[++i];
+        } else if (arg == "-h" || arg == "--help") {
+            print_usage(argv[0]);
+            return std::nullopt;
         } else if (arg == "-v" || arg == "--verbose") {
             opts.verbose = true;
         } else if (arg == "-w" || arg == "--width") {
@@ -89,7 +104,8 @@ std::optional<CmdOptions> parse_args(int argc, char** argv) {
     }
 
     if (opts.input_filename.empty()) {
-        std::cout << "Error: No input file specified.\n";
+        std::cout << "Error: No input file specified.\n\n";
+        print_usage(argv[0]);
         return std::nullopt;
     }
 
