05dff2ffb01a30821becd2ec822ab0bbc13b7606
Miroslav Demek <xdemek@fi.muni.cz>
2022-07-18 22:25:42 +0200

    Make interface of SvgImage consistent with StbImage

--- src/drawing/svg.hpp
+++ src/drawing/svg.hpp
@@ -9,28 +9,22 @@
 
 struct SvgImage {
 
-    SvgImage(size_t width, size_t height) : max_x(width), max_y(height) {}
-
-    void draw_rectangle(Rect rect, const Style& style) {
-        svg_str << "<rect ";
-
-        svg_str << "x='" << rect.x << "' y='" << rect.y << "' ";
-        svg_str << "width='" << rect.width << "' height='" << rect.height << "' ";
-        svg_str << "fill='" << style.fill_color << "' ";
-
-        if (style.fill_opacity < 1) {
-            svg_str << "fill-opacity='" << style.fill_opacity << "' ";
-        }
-
-        if (style.border_width != 0) {
-            svg_str << "stroke='" << style.border_color << "' ";
-            svg_str << "stroke-width='" << style.border_width << "' ";
-        }
-
-        svg_str << "/>\n";
+    SvgImage(size_t width, size_t height, Rgb background = { 255, 255, 255 })
+        : width_(width),
+          height_(height)
+    {
+        draw_rectangle({0, 0, width, height}, background);
     }
 
-    void save(const std::string& path, size_t width_in_pixels, size_t height_in_pixels) const {
+    void draw_rectangle(Rect rect, Rgb color) {
+        svg_str_ << "<rect ";
+        svg_str_ << "x='" << rect.x << "' y='" << rect.y << "' ";
+        svg_str_ << "width='" << rect.width << "' height='" << rect.height << "' ";
+        svg_str_ << "fill='" << color.to_string() << "' ";
+        svg_str_ << "/>\n";
+    }
+
+    void save(const std::string& path) const {
         std::ofstream out(path);
 
         if (!out) {
@@ -38,55 +32,55 @@ struct SvgImage {
         }
 
         out << "<svg xmlns='http://www.w3.org/2000/svg'\n";
-        out << "\twidth='" << width_in_pixels << "px' height='" << height_in_pixels << "px'\n";
-        out << "\tviewBox='0 0 " << max_x << " " << max_y << "'>\n";
-        out << svg_str.str();
+        out << "\twidth='" << width_ << "px' height='" << height_ << "px'\n";
+        out << "\tviewBox='0 0 " << width_ << " " << height_ << "'>\n";
+        out << svg_str_.str();
         out << "</svg>\n";
     }
 
+    size_t width() const {
+        return width_;
+    }
+
+    size_t height() const {
+        return height_;
+    }
+
 private:
+    std::stringstream svg_str_;
 
-    std::stringstream svg_str;
-
-    size_t max_x;
-    size_t max_y;
+    size_t width_;
+    size_t height_;
 };
 
 
 struct SvgDrawer : ImageDrawer {
 
     void operator()(const Grid& grid, const ImageConfig& config) override {
-        SvgImage image(config.width, config.height);
+        SvgImage image(config.width, config.height, { 255, 255, 255 });
 
-        draw_background(image, config);
         draw_borders(image, config);
         draw_grid(image, config, grid);
 
-        image.save(config.path, config.width, config.height);
+        image.save(config.path);
     }
 
 private:
-
-    void draw_background(SvgImage& image, const ImageConfig& config) {
-        Style style { "white", 1, "black", 0 };
-        image.draw_rectangle({0, 0, config.width, config.height}, style);
-    }
-
     void draw_borders(SvgImage& image, const ImageConfig& config) {
-        Style border_style { "black", 1, "black", 0 };
+        Rgb border_color = { 0, 0, 0 };
 
         size_t w = config.width;
         size_t h = config.height;
         size_t b = config.border_size;
 
-        image.draw_rectangle({ 0, 0, w, b }, border_style);
-        image.draw_rectangle({ 0, 0, b, h }, border_style);
-        image.draw_rectangle({ 0, h - b, w, b }, border_style);
-        image.draw_rectangle({ w - b, 0, b, h }, border_style);
+        image.draw_rectangle({ 0, 0, w, b }, border_color);
+        image.draw_rectangle({ 0, 0, b, h }, border_color);
+        image.draw_rectangle({ 0, h - b, w, b }, border_color);
+        image.draw_rectangle({ w - b, 0, b, h }, border_color);
     }
 
     void draw_grid(SvgImage& image, const ImageConfig& config, const Grid& grid) {
-        Style style { "black", 1, "black", 0 };
+        Rgb color;
         Rect block_rect = { 0, config.border_size, config.block_size, config.block_size };
 
         size_t max_occupancy = config.adjust_colors ? grid.max_occupancy() : grid.block_capacity();
@@ -99,9 +93,9 @@ private:
                     continue;
                 }
                 float density = grid.count_at(row, col)/(float)max_occupancy;
-                style.fill_color = config.color_palette.sample_color(density).to_string();
+                color = config.color_palette.sample_color(density);
 
-                image.draw_rectangle(block_rect, style);
+                image.draw_rectangle(block_rect, color);
 
                 block_rect.x += config.block_size;
             }
